a39c19c53fe3a4c28054e6b9c55223301577d838
ashidesu <147083825+ashidesu@users.noreply.github.com>
2025-09-01 04:53:41 +0800

    Update main.js

--- Components/main.js
+++ Components/main.js
@@ -1,6 +1,6 @@
 import leftNavigation from '../Components/SideBar.js';
 import feed from '../Components/videoBox.js';
-import rightNavigation from './rightNav.js';
+import rightNavigation from '../Components/rightNav.js';
 {customElements.define('right-nav',rightNavigation);}
 {customElements.define('left-nav', leftNavigation);}
 {customElements.define('video-feed', feed);}
